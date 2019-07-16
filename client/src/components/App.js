9a325f986afb0d9c33f99d70024e6e9dbc5076bb
Vinh Tran <vincen7.tran@gmail.com>
2019-07-16 16:22:09 -0700

    React Router DOM Setup

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import StreamCreate from '../components/streams/StreamCreate';
+import StreamEdit from '../components/streams/StreamEdit';
+import StreamDelete from '../components/streams/StreamDelete';
+import StreamList from '../components/streams/StreamList';
+import StreamShow from '../components/streams/StreamShow';
+
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <BrowserRouter>
+       <div>
+        <Route path="/" exact component={StreamList} />
+        <Route path="/streams/new" component={StreamCreate} />
+        <Route path="/streams/edit" component={StreamEdit} />
+        <Route path="/streams/delete" component={StreamDelete} />
+        <Route path="/streams/show" component={StreamShow} />
+       </div>
+      </BrowserRouter>
+    </div>
   );
 };
 
