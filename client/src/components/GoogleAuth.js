3f819cbd5b6a140d5073640fd4ebf04543804779
Vinh Tran <vincen7.tran@gmail.com>
2019-07-18 12:36:53 -0700

    renders auth change

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -2,18 +2,42 @@ import React from 'react';
 import clientId from '../clientId';
 
 class GoogleAuth extends React.Component {
+  state = {
+    isSignedIn: null
+  };
+
   componentDidMount() {
     window.gapi.load('client:auth2', () => {
       window.gapi.client.init({
         clientId,
         scope: 'email'
+      }).then(() => {
+        this.auth = window.gapi.auth2.getAuthInstance();
+        this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+        this.auth.isSignedIn.listen(this.onAuthChange);
       });
     });
   }
 
+  onAuthChange = () => {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  };
+
+  renderAuthButton() {
+    const { isSignedIn } = this.state;
+
+    if (isSignedIn === null) {
+      return null;
+    } else if (isSignedIn) {
+      return <div>I am signed in!</div>;
+    } else {
+      return <div>Not signed in</div>;
+    }
+  }
+
   render() {
     return (
-      <div>Google</div>
+      <div>{this.renderAuthButton()}</div>
     );
   }
 }
